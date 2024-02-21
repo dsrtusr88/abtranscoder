e24811feba1e66452e031666611a484e21d2f40e
dsrtusr88 <33256205+dsrtusr88@users.noreply.github.com>
2024-02-20 21:56:22 -0500

    Update redactedapi.py

--- redactedapi.py
+++ redactedapi.py
@@ -210,130 +210,28 @@ class RedactedAPI:
                 #print("Invalid input. Please enter 'y' for yes, 'n' for no, or 'q' to quit.")
 
 
-    def upload(self, group, torrent, new_torrent, bitrate, description=[], promptuser=False):
-        torrent_data = open(new_torrent, 'rb').read()
-        torrentfilebasename=os.path.basename(new_torrent)
-        # this makes a list of artist records.. not sure if the API supports this instead of names
-        #all_artists=group['group']['musicInfo']['artists'] + group['group']['musicInfo']['with'] + group['group']['musicInfo']['composers'] \
-        #    + group['group']['musicInfo']['conductor'] + group['group']['musicInfo']['dj'] + group['group']['musicInfo']['remixedBy'] + group['group']['musicInfo']['producer']
-        
-        # Create a list of all artists' names
-        all_artists = [artist['name'] for artist in group['group']['musicInfo']['artists']]
-        all_artists += [artist['name'] for artist in group['group']['musicInfo']['with']]
-        all_artists += [artist['name'] for artist in group['group']['musicInfo']['composers']]
-        all_artists += [artist['name'] for artist in group['group']['musicInfo']['conductor']]
-        all_artists += [artist['name'] for artist in group['group']['musicInfo']['dj']]
-        all_artists += [artist['name'] for artist in group['group']['musicInfo']['remixedBy']]
-        all_artists += [artist['name'] for artist in group['group']['musicInfo']['producer']]
+    def upload(self, group, torrent, new_torrent, format, description=[]):
+        url = "https://redacted.ch/upload.php?groupid=%s" % group['group']['id']
+        response = self.session.get(url)
+        forms = mechanize.ParseFile(StringIO(response.text.encode('utf-8')), url)
+        form = forms[-1]
+        form.find_control('file_input').add_file(open(new_torrent), 'application/x-bittorrent', os.path.basename(new_torrent))
+        #if torrent['remastered']:
+        #    form.find_control('remaster').set_single('1')
+        form['remaster_year'] = str(torrent['remasterYear'])
+        form['remaster_title'] = torrent['remasterTitle']
+        form['remaster_record_label'] = torrent['remasterRecordLabel']
+        form['remaster_catalogue_number'] = torrent['remasterCatalogueNumber']
+        form.find_control('format').set('1', formats[format]['format'])
+        form.find_control('bitrate').set('1', formats[format]['encoding'])
+        form.find_control('media').set('1', torrent['media'])
 
-
-        artist_importance_array = [0 for _ in group['group']['musicInfo']['artists']] \
-            + [1 for _ in group['group']['musicInfo']['with']] \
-            + [2 for _ in group['group']['musicInfo']['composers']] \
-            + [3 for _ in group['group']['musicInfo']['conductor']] \
-            + [4 for _ in group['group']['musicInfo']['conductor']] \
-            + [5 for _ in group['group']['musicInfo']['dj']] \
-            + [6 for _ in group['group']['musicInfo']['remixedBy']] \
-            + [7 for _ in group['group']['musicInfo']['producer']] 
-        isunknown = (group['group']['releaseType'] == 21) # 21 = unknown
-        format="MP3"
-        #filetype = group['group']['categoryId'] # group says categoryName=Music and categoryId=1 but supposed to be type=0?
-        if (group['group']['categoryName']=='Music'):
-            filetype=0
-        else:
-            print(f"Unknown category {group['group']['categoryName']}")
-            raise Exception
-        
-        if (bitrate == "V0"):
-            bitrate= "V0 (VBR)"
-        # Convert the list of tags into a comma-separated string
-        tags_string = ', '.join(group['group']['tags'])
         release_desc = '\n'.join(description)
-        if (promptuser):
-            # Gather information for the summary
-            summary = f"Group ID: {group['group']['id']}\n" \
-                    f"New Torrent File: {os.path.basename(new_torrent)}\n" \
-                    f"Type: {filetype}\n" \
-                    f"Artists: {all_artists}\n" \
-                    f"Importance: {artist_importance_array}\n" \
-                    f"Title: {group['group']['name']}\n" \
-                    f"Year: {group['group']['year']}\n" \
-                    f"Release Type: {group['group']['releaseType']}\n" \
-                    f"Remaster Year: {torrent['remasterYear']}\n" \
-                    f"Remaster Title: {torrent['remasterTitle']}\n" \
-                    f"Remaster Record Label: {torrent['remasterRecordLabel']}\n" \
-                    f"Remaster Catalogue Number: {torrent['remasterCatalogueNumber']}\n" \
-                    f"Scene: {torrent['scene']}\n" \
-                    f"Format: {format}\n" \
-                    f"Bitrate: {bitrate}\n" \
-                    f"Vanity House: {group['group']['vanityHouse']}\n" \
-                    f"Media: {torrent['media']}\n" \
-                    f"Tags: {tags_string}\n" \
-                    f"Image: {group['group']['wikiImage']}\n" \
-                    f"Album Description: {group['group']['bbBody']}\n" \
-                    f"Release Description: {release_desc}" \
-                    f"Unknown: {isunknown}\n" 
+        if release_desc:
+            form['release_desc'] = release_desc
 
-                    # #unused fields:
-                    # other_bitrate=
-                    # vbr=
-                    # logfiles=
-                    # extra_file_#=
-                    # extra_format[]=
-                    # extra_bitrate[]=
-                    # extra_release_desc[]=
-                    # desc= (str) Description for non-music torrents
-                    # requestid=(int) requestID being filled
-            # Prompt user for confirmation
-            #if promptuser:
-                #if not self.prompt_user_confirmation(summary):
-                    #print("Upload canceled.")
-                    #return
-        
-        try:
-            request_params = {
-                'newfiles': {'file_input': (torrentfilebasename, torrent_data)},
-                'groupid': group['group']['id'],
-                'type': filetype,
-                'artists': all_artists,
-                'importance': artist_importance_array,
-                'title': group['group']['name'],
-                'year': group['group']['year'],
-                'releasetype': group['group']['releaseType'],
-                'remaster_year': torrent['remasterYear'],
-                'remaster_title': torrent['remasterTitle'],
-                'remaster_record_label': torrent['remasterRecordLabel'],
-                'remaster_catalogue_number': torrent['remasterCatalogueNumber'],
-                'format': format,
-                'bitrate': bitrate,
-                'vanity_house': group['group']['vanityHouse'],
-                'media': torrent['media'],
-                'tags': tags_string,
-                'image': group['group']['wikiImage'],
-                'album_desc': group['group']['bbBody'],
-                'release_desc': release_desc
-            }
-            # Add 'scene' to request parameters only if it's True
-            if torrent['scene']:
-                request_params['scene'] = torrent['scene']
-            if isunknown:
-                request_params['unknown'] = True
-
-            # Make the POST request
-            res = self.request_post("upload", **request_params)
-            return res                
-        except requests.ConnectionError as e:
-            print("Error: Network problem (e.g., DNS failure, refused connection, etc)")
-            raise e
-        except requests.Timeout as e:
-            print("Error: Request timed out")
-            raise e
-        except requests.HTTPError as http_err:
-            print(f"HTTP error occurred: {http_err}")
-            raise http_err
-        except Exception as err:
-            print(f"An error occurred: {err}")
-            raise err
+        _, data, headers = form.click_request_data()
+        return self.session.post(url, data=data, headers=dict(headers))
 
     def set_24bit(self, torrent):
         url = "https://redacted.ch/torrents.php?action=edit&id=%s" % torrent['id']
